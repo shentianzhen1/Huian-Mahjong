516c660d71155fc251481fd1182242887c7218ae
shentianzhen1 <83879142+shentianzhen1@users.noreply.github.com>
2026-09-18 19:08:27 +0800

    Export eight-hand MatchRunner API

--- workspace/simulator/__init__.py
+++ workspace/simulator/__init__.py
@@ -2,7 +2,11 @@
 from .core import RandomAgent, SimulationResult, Simulator, SimulatorConfig, make_wall
 from .evaluation import BatchEvaluation, HandSummary, run_many_normal_hands
 from .match import MatchProgressState, MatchScoreState, score_eight_hand_match
+from .match_runner import (MatchHandContext, MatchHandRecord, MatchHandResult,
+                           MatchRunResult, MatchRunner, run_eight_hand_match)
 
 __all__ = ["RandomAgent", "SimulationResult", "Simulator", "SimulatorConfig", "make_wall",
            "BatchEvaluation", "HandSummary", "run_many_normal_hands",
-           "MatchProgressState", "MatchScoreState", "score_eight_hand_match"]
+           "MatchProgressState", "MatchScoreState", "score_eight_hand_match",
+           "MatchHandContext", "MatchHandRecord", "MatchHandResult",
+           "MatchRunResult", "MatchRunner", "run_eight_hand_match"]
