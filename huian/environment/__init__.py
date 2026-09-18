11e451bec7c66ff12d12b66e5aa1aa985045e0de
shentianzhen1 <83879142+shentianzhen1@users.noreply.github.com>
2026-09-18 12:53:56 +0800

    Hook current-player qiangjin transitions into Environment._apply

--- huian/environment/__init__.py
+++ huian/environment/__init__.py
@@ -1,4 +1,15 @@
 from .state import HuianGameState
 from .engine import HuianEnvironment, DeadLoopError
+from .special_apply import apply_qiangjin_action
+
+_orig_apply = HuianEnvironment._apply.__func__
+
+@staticmethod
+def _apply_with_qiangjin(state, action):
+    if apply_qiangjin_action(state, action):
+        return None
+    return _orig_apply(state, action)
+
+HuianEnvironment._apply = _apply_with_qiangjin
 
 __all__ = ["HuianGameState", "HuianEnvironment", "DeadLoopError"]
