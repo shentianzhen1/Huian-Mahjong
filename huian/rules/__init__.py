b4a3569209ab036311c8a1de71270ea5bddc132a
shentianzhen1 <83879142+shentianzhen1@users.noreply.github.com>
2026-09-18 23:50:06 +0800

    Export special outcome registry

--- huian/rules/__init__.py
+++ huian/rules/__init__.py
@@ -5,6 +5,8 @@ from .fan import FanAggregator, FanComponent, FanResult
 from .context import (DrawSource, HuContext, KongKind, SanjindaoChoice,
                       SanjindaoDecision, WinSource, YoujinStage)
 from .observed_settlement import HuianObservedSettlement, HuianObservedSettlementPlugin
+from .special_outcomes import (SPECIAL_OUTCOMES, SpecialOutcomeProfile,
+                               special_outcome_profile)
 from .adapter import HuianRulesAdapter
 
 __all__ = ["DrawSource", "FanAggregator", "FanComponent", "FanResult", "HuContext", "HuDecomposition", "HuianRules", "HuResult",
@@ -12,4 +14,5 @@ __all__ = ["DrawSource", "FanAggregator", "FanComponent", "FanResult", "HuContex
            "SanjindaoDecision", "WinSource", "EvidenceStatus",
            "UnknownRuleError", "UNKNOWN_RULES", "KongFanResult", "Settlement", "YoujinScoreTerms",
            "YoujinStage",
-           "HuianObservedSettlement", "HuianObservedSettlementPlugin"]
+           "HuianObservedSettlement", "HuianObservedSettlementPlugin",
+           "SPECIAL_OUTCOMES", "SpecialOutcomeProfile", "special_outcome_profile"]
