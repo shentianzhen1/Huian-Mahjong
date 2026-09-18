f7bed6c1bcb7628aa5c4c9cffb88fa3b44549404
shentianzhen1 <83879142+shentianzhen1@users.noreply.github.com>
2026-09-18 17:54:17 +0800

    Export eight-hand match scoring

--- workspace/simulator/__init__.py
+++ workspace/simulator/__init__.py
@@ -1,6 +1,8 @@
 """Rule-neutral simulator scaffolding."""
 from .core import RandomAgent, SimulationResult, Simulator, SimulatorConfig, make_wall
 from .evaluation import BatchEvaluation, HandSummary, run_many_normal_hands
+from .match import MatchScoreState, score_eight_hand_match
 
 __all__ = ["RandomAgent", "SimulationResult", "Simulator", "SimulatorConfig", "make_wall",
-           "BatchEvaluation", "HandSummary", "run_many_normal_hands"]
+           "BatchEvaluation", "HandSummary", "run_many_normal_hands",
+           "MatchScoreState", "score_eight_hand_match"]
