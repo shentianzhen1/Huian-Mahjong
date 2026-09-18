92c3b7ab47572f7853ac951cb439a3a4eb819f4f
shentianzhen1 <83879142+shentianzhen1@users.noreply.github.com>
2026-09-18 18:45:04 +0800

    Export FanAggregator V0.1

--- huian/rules/__init__.py
+++ huian/rules/__init__.py
@@ -1,12 +1,13 @@
 from .config import RulesConfig, EvidenceStatus, UnknownRuleError, UNKNOWN_RULES
 from .engine import (HuDecomposition, HuianRules, HuResult, KongFanResult,
                      Settlement, YoujinScoreTerms)
+from .fan import FanAggregator, FanComponent, FanResult
 from .context import (DrawSource, HuContext, KongKind, SanjindaoChoice,
                       SanjindaoDecision, WinSource, YoujinStage)
 from .observed_settlement import HuianObservedSettlement, HuianObservedSettlementPlugin
 from .adapter import HuianRulesAdapter
 
-__all__ = ["DrawSource", "HuContext", "HuDecomposition", "HuianRules", "HuResult",
+__all__ = ["DrawSource", "FanAggregator", "FanComponent", "FanResult", "HuContext", "HuDecomposition", "HuianRules", "HuResult",
            "HuianRulesAdapter", "KongKind", "RulesConfig", "SanjindaoChoice",
            "SanjindaoDecision", "WinSource", "EvidenceStatus",
            "UnknownRuleError", "UNKNOWN_RULES", "KongFanResult", "Settlement", "YoujinScoreTerms",
