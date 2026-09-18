48c6afc732f2fb65b85493a88efbfac9d19cc62d
shentianzhen1 <83879142+shentianzhen1@users.noreply.github.com>
2026-09-18 21:04:40 +0800

    Export match rule-gap summary utility

--- workspace/simulator/__init__.py
+++ workspace/simulator/__init__.py
@@ -5,10 +5,11 @@ from .match import MatchProgressState, MatchScoreState, score_eight_hand_match
 from .match_runner import (MatchHandContext, MatchHandRecord, MatchHandResult,
                            MatchRunResult, MatchRunner, run_eight_hand_match,
                            run_real_ordinary_match)
+from .unknowns import summarize_match_rule_gaps
 
 __all__ = ["RandomAgent", "SimulationResult", "Simulator", "SimulatorConfig", "make_wall",
            "BatchEvaluation", "HandSummary", "run_many_normal_hands",
            "MatchProgressState", "MatchScoreState", "score_eight_hand_match",
            "MatchHandContext", "MatchHandRecord", "MatchHandResult",
            "MatchRunResult", "MatchRunner", "run_eight_hand_match",
-           "run_real_ordinary_match"]
+           "run_real_ordinary_match", "summarize_match_rule_gaps"]
