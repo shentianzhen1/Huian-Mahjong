65bb2ec4c970ce2ff2c4557c0e7729572d53e827
shentianzhen1 <83879142+shentianzhen1@users.noreply.github.com>
2026-09-18 19:04:45 +0800

    Export dealer-aware match progression

--- workspace/simulator/__init__.py
+++ workspace/simulator/__init__.py
@@ -1,8 +1,8 @@
 """Rule-neutral simulator scaffolding."""
 from .core import RandomAgent, SimulationResult, Simulator, SimulatorConfig, make_wall
 from .evaluation import BatchEvaluation, HandSummary, run_many_normal_hands
-from .match import MatchScoreState, score_eight_hand_match
+from .match import MatchProgressState, MatchScoreState, score_eight_hand_match
 
 __all__ = ["RandomAgent", "SimulationResult", "Simulator", "SimulatorConfig", "make_wall",
            "BatchEvaluation", "HandSummary", "run_many_normal_hands",
-           "MatchScoreState", "score_eight_hand_match"]
+           "MatchProgressState", "MatchScoreState", "score_eight_hand_match"]
