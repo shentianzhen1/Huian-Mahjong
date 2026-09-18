71ed06307a5ae58df13d12b0d33d6d3f614f9699
shentianzhen1 <83879142+shentianzhen1@users.noreply.github.com>
2026-09-18 19:11:52 +0800

    Export real ordinary eight-hand match runner

--- workspace/simulator/__init__.py
+++ workspace/simulator/__init__.py
@@ -3,10 +3,12 @@ from .core import RandomAgent, SimulationResult, Simulator, SimulatorConfig, mak
 from .evaluation import BatchEvaluation, HandSummary, run_many_normal_hands
 from .match import MatchProgressState, MatchScoreState, score_eight_hand_match
 from .match_runner import (MatchHandContext, MatchHandRecord, MatchHandResult,
-                           MatchRunResult, MatchRunner, run_eight_hand_match)
+                           MatchRunResult, MatchRunner, run_eight_hand_match,
+                           run_real_ordinary_match)
 
 __all__ = ["RandomAgent", "SimulationResult", "Simulator", "SimulatorConfig", "make_wall",
            "BatchEvaluation", "HandSummary", "run_many_normal_hands",
            "MatchProgressState", "MatchScoreState", "score_eight_hand_match",
            "MatchHandContext", "MatchHandRecord", "MatchHandResult",
-           "MatchRunResult", "MatchRunner", "run_eight_hand_match"]
+           "MatchRunResult", "MatchRunner", "run_eight_hand_match",
+           "run_real_ordinary_match"]
