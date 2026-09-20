7c17326dfea58d5f40a9bdb00cf697d03b1df4e7
shentianzhen1 <83879142+shentianzhen1@users.noreply.github.com>
2026-09-20 14:38:40 +0800

    Expose Youjin progression API

--- huian/__init__.py
+++ huian/__init__.py
@@ -3,16 +3,19 @@ from .rules import (DrawSource, FanAggregator, FanComponent, FanResult, HuContex
                     HuianRulesAdapter, HuResult, KongFanResult, KongKind, RulesConfig,
                     SanjindaoChoice, SanjindaoDecision, SPECIAL_OUTCOMES,
                     SpecialOutcomeProfile, UnknownRuleError, WinSource,
-                    YoujinOfferRule, YoujinOpponentResponseRule, YoujinScoreTerms,
+                    YoujinOfferRule, YoujinOpponentResponseRule,
+                    YoujinProgressionRule, YoujinScoreTerms,
                     YoujinStage, special_outcome_profile, youjin_offer_rule,
-                    youjin_opponent_response_rule)
+                    youjin_opponent_response_rule, youjin_progression_rule)
 from .environment import HuianGameState, HuianEnvironment, DeadLoopError
 
 __all__ = ["DrawSource", "FanAggregator", "FanComponent", "FanResult", "HuContext", "HuDecomposition", "HuianRules",
            "HuianRulesAdapter", "HuResult", "KongFanResult", "KongKind", "RulesConfig",
            "SanjindaoChoice", "SanjindaoDecision", "UnknownRuleError", "WinSource",
-           "YoujinOfferRule", "YoujinOpponentResponseRule", "YoujinScoreTerms",
+           "YoujinOfferRule", "YoujinOpponentResponseRule",
+           "YoujinProgressionRule", "YoujinScoreTerms",
            "YoujinStage", "youjin_offer_rule", "youjin_opponent_response_rule",
+           "youjin_progression_rule",
            "SPECIAL_OUTCOMES",
            "SpecialOutcomeProfile", "special_outcome_profile",
            "HuianGameState", "HuianEnvironment",
