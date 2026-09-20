548c3545c2990e564676fe1199eb11f6423dc1b3
shentianzhen1 <83879142+shentianzhen1@users.noreply.github.com>
2026-09-20 12:36:41 +0800

    Expose Youjin response rule at package level

--- huian/__init__.py
+++ huian/__init__.py
@@ -3,13 +3,15 @@ from .rules import (DrawSource, FanAggregator, FanComponent, FanResult, HuContex
                     HuianRulesAdapter, HuResult, KongFanResult, KongKind, RulesConfig,
                     SanjindaoChoice, SanjindaoDecision, SPECIAL_OUTCOMES,
                     SpecialOutcomeProfile, UnknownRuleError, WinSource,
-                    YoujinScoreTerms, YoujinStage, special_outcome_profile)
+                    YoujinOpponentResponseRule, YoujinScoreTerms, YoujinStage,
+                    special_outcome_profile, youjin_opponent_response_rule)
 from .environment import HuianGameState, HuianEnvironment, DeadLoopError
 
 __all__ = ["DrawSource", "FanAggregator", "FanComponent", "FanResult", "HuContext", "HuDecomposition", "HuianRules",
            "HuianRulesAdapter", "HuResult", "KongFanResult", "KongKind", "RulesConfig",
            "SanjindaoChoice", "SanjindaoDecision", "UnknownRuleError", "WinSource",
-           "YoujinScoreTerms", "YoujinStage", "SPECIAL_OUTCOMES",
+           "YoujinOpponentResponseRule", "YoujinScoreTerms", "YoujinStage",
+           "youjin_opponent_response_rule", "SPECIAL_OUTCOMES",
            "SpecialOutcomeProfile", "special_outcome_profile",
            "HuianGameState", "HuianEnvironment",
            "DeadLoopError"]
